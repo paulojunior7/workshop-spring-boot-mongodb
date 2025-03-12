ed688297a3d2c087bec74c9e65b0030c8d6c81e4
paulojunior7 <122480101+paulojunior7@users.noreply.github.com>
2025-03-12 14:42:24 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/udemy/workshopmongo/services/UserService.java
@@ -0,0 +1,22 @@
+package com.udemy.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemy.workshopmongo.domain.User;
+import com.udemy.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+		
+	}
+
+}
