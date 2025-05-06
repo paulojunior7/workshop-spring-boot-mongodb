4adc29073c9b307e38d73ff488f41c395ffa6ee0
paulojunior7 <122480101+paulojunior7@users.noreply.github.com>
2025-05-06 13:25:06 -0300

    Insercao de usuario com POST

--- src/main/java/com/udemy/workshopmongo/services/UserService.java
+++ src/main/java/com/udemy/workshopmongo/services/UserService.java
@@ -6,22 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.udemy.workshopmongo.domain.User;
+import com.udemy.workshopmongo.dto.UserDTO;
 import com.udemy.workshopmongo.repository.UserRepository;
 import com.udemy.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
-		
+
 	}
 
 	public User findById(String id) {
-		return repo.findById(id)
-				.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
 }
