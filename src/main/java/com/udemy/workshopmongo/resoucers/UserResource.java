f2820dba0f7d4927d3e4a8c790bb93b0376c2222
paulojunior7 <122480101+paulojunior7@users.noreply.github.com>
2025-05-08 01:00:56 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/udemy/workshopmongo/resoucers/UserResource.java
+++ src/main/java/com/udemy/workshopmongo/resoucers/UserResource.java
@@ -6,9 +6,11 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -40,7 +42,7 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
-	
+	 
 	@PostMapping
 	public ResponseEntity<UserDTO> insert(@RequestBody UserDTO objDto) {
 		User obj = service.fromDTO(objDto);
@@ -49,4 +51,9 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 		
 	}
+		 @DeleteMapping("/{id}")
+		public ResponseEntity<Void> delete(@PathVariable String id) {
+			service.delete(id);
+			return ResponseEntity.noContent().build();
+		}
 }
